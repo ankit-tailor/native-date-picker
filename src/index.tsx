39406378b35067c88d92cc0d142c04c2633f6a61
ankit-tailor <tailorankit3345@gmail.com>
2022-07-28 13:53:49 +0530

    feat: time picker

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,4 @@
-import { NativeModules, Platform } from 'react-native';
+import { Platform } from 'react-native';
+import { DateTimePicker } from './DateTimePicker';
 
-const LINKING_ERROR =
-  `The package 'native-date-picker-module' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
-
-const NativeDatePicker = NativeModules.NativeDatePicker
-  ? NativeModules.NativeDatePicker
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-export function multiply(a: number, b: number): Promise<number> {
-  return NativeDatePicker.multiply(a, b);
-}
-
-export function getDatePicker(): Promise<any> {
-  return NativeDatePicker.getDatePicker();
-}
+export { DateTimePicker };
